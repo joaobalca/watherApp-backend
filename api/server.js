1261eefe7f02ae9b1636fa8a974c82b3ba91d1cc
joaobalca <joaobalca@outsmartis.com>
2025-02-01 18:47:04 +0000

    added cors

--- api/server.js
+++ api/server.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectDB();
